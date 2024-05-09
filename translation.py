dccccb2becb4ed16c226e93effb313d40634f144
Rahuly0272 <iitjeerahul2019@gmail.com>
2024-05-09 23:53:39 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,13 @@
 class Translation(object):
 
       # Start text
-      START = "<b>Hi 👋 I Am A Channel Auto Forward Bot of <a href='https://t.me/VJ_Botz'>VJ Botz</a><b>. \n<b>Make your own bot now 👉 <a href='https://youtube.com/@Tech_VJ'>Tutorial Video</a><b> \n\n<b>Support Channel and Groups:</b>\n @VJ_Bots, @VJ_Bot_disscussion \n\n<b>About Bot:</b>\n Give Command /about"             
+      START = "<b>Hi 👋 I Am A Channel Auto Forward Bot of <a href='https://t.me/RS_Movie'>RAS Botz</a><b>. \n<b>Make sure to join channel 👉 <a href='https://t.me/RS_Movie'>BackUP Channel</a><b> \n\n<b>Support Channel and Groups:</b>\n @Anonymous_7, @RS_Movie \n\n<b>About Bot:</b>\n Give Command /about"             
 
       #About text
       ABOUT = """<b>⍟───[ MY ᴅᴇᴛᴀɪʟꜱ ]───⍟
-‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/VJ_Botz>VJ Auto Forward Bot 🇮🇳</a>
+‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/RS_Movie>RAS Forward Bot 🇮🇳</a>
  ‣ My best friend : <a href='tg://settings'>ᴛʜɪs ᴘᴇʀsᴏɴ</a> 
- ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/kingvj01'>Tech VJ 🇮🇳</a> 
+ ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/RS_Movie'>Tech RAS 🇮🇳</a> 
  ‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ</a> 
  ‣ ʟᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>ᴘʏᴛʜᴏɴ 3</a> 
  ‣ ᴅᴀᴛᴀ ʙᴀsᴇ : <a href='https://www.mongodb.com/'>ᴍᴏɴɢᴏ ᴅʙ</a> 
